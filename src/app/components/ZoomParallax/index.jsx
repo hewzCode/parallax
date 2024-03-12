2d0de113a5ed54c3d7ca48c90d0be000926e99ee
huz <hewzcode@gmail.com>
2024-03-12 14:36:28 +0100

    first commit

---
+++ src/app/components/ZoomParallax/index.jsx
@@ -0,0 +1,66 @@
+import styles from './styles.module.scss';
+import original from '../../../../public/images/original.png';
+import lucy from '../../../../public/images/lucy.png';
+import moonback from '../../../../public/images/moonback.png';
+import rocks from '../../../../public/images/rocks.png'
+import spaceback from '../../../../public/images/spaceback.png'
+import david from '../../../../public/images/david.png'
+import Image from 'next/image';
+import { useScroll, useTransform, motion} from 'framer-motion';
+import { useRef } from 'react';
+
+export default function Index() {
+    
+    const container = useRef(null);
+    const { scrollYProgress } = useScroll({
+        target: container,
+        offset: ['start start', 'end end']
+    })
+
+    const scale4 = useTransform(scrollYProgress, [0, 1], [1, 4]);
+    const scale5 = useTransform(scrollYProgress, [0, 1], [1, 5]);
+    const scale6 = useTransform(scrollYProgress, [0, 1], [1, 6]);
+    const scale8 = useTransform(scrollYProgress, [0, 1], [1, 8]);
+    const scale9 = useTransform(scrollYProgress, [0, 1], [1, 9]);
+
+    const pictures = [
+        {
+            src: original,
+            scale: scale4
+        },
+        {
+            src: lucy,
+            scale: scale6
+        },
+        {
+            src: rocks,
+            scale: scale5
+        },
+        {
+            src: david,
+            scale: scale6
+        },
+       
+    ]
+
+    return (
+        <div ref={container} className={styles.container}>
+            <div className={styles.sticky}>
+                {
+                    pictures.map( ({src, scale}, index) => {
+                        return <motion.div key={index} style={{scale}} className={styles.el}>
+                            <div className={styles.imageContainer}>
+                                <Image
+                                    src={src}
+                                    fill
+                                    alt="image"
+                                    placeholder='blur'
+                                />
+                            </div>
+                        </motion.div>
+                    })
+                }
+            </div>
+        </div>
+    )
+}
